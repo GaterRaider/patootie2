9b58b4d1e7977f01b33d4550717e0d24b5f0d1e4
GaterRaider <julian.peuker@outlook.de>
2025-11-24 18:12:03 +0100

    Attempt to fix raw json

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -58,10 +58,11 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, { index: false }));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
+    res.set("Content-Type", "text/html");
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
